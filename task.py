b40335e5c4dfd5befe2287e8c75d13d5c5d08d78
Мендыгулов Нуркен Нурланович <MendygulovN@lcoast.itstep>
2023-10-17 21:52:11 +0600

    first commit

---
+++ task.py
@@ -0,0 +1,13 @@
+numLst = [1,2,3,4,5,6,7,8,9,0]
+strLst = ["Apple","Avocado","Banana","Antarctida","Computer"]
+
+sortedLstNum = list(filter(lambda x: x%2==0, numLst))
+numLstsquare = list(filter(lambda x: x*x, sortedLstNum))
+#print(sortedLstNum)
+#print(numLstsquare)
+
+
+
+
+
+
