fe8edee61e74c474e8d17c3db0896aa4de2ced68
Hypkeh <144148068+Hypkeh@users.noreply.github.com>
2023-11-02 21:46:41 +0600

    Add files via upload

--- task.py
+++ task.py
@@ -1,13 +1,20 @@
-numLst = [1,2,3,4,5,6,7,8,9,0]
-strLst = ["Apple","Avocado","Banana","Antarctida","Computer"]
+import os
 
-sortedLstNum = list(filter(lambda x: x%2==0, numLst))
-numLstsquare = list(filter(lambda x: x*x, sortedLstNum))
-print(sortedLstNum)
-#print(numLstsquare)
+#1
+os.mkdir("C:\Users\MendygulovN\Desktop\dsdasd\watch_me")
 
 
 
+#2
+os.rename("C:\Users\MendygulovN\Desktop\dsdasd\Nina_Stoletova.jpg", "C:\Users\MendygulovN\Desktop\dsdasd\Stoletova_Nina.jpg")
+os.rename("C:\Users\MendygulovN\Desktop\dsdasd\Misha_Perelman.jpg", "C:\Users\MendygulovN\Desktop\dsdasd\Perelman_Misha.jpg")
 
+#3
+list = os.listdir("C:\Users\MendygulovN\Desktop\dsdasd\list")
+print(list)
+
+with open('list.txt', 'w') as file:
+    file.write(list)
+    
 
 
